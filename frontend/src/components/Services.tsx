3f650345e2d9b74143d830f16ae30dc5e75f1a93
SudhansuuRanjan <sudhanshuranjan2k18@gmail.com>
2023-06-04 00:31:08 +0530

    first commit

---
+++ frontend/src/components/Services.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Heading from './Heading'
+
+const Services = () => {
+  return (
+    <div className='bg-[#04000d]'>
+        <Heading title="Services" subtitle='ASPECT VISION' description='Lorem Ipsum is simply dummy text of the printing and typesetting industry.' />
+    </div>
+  )
+}
+
+export default Services
